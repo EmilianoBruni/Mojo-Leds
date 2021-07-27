176f706bfc841bd004832284150cc1ae1eaa8199
ebruni <info@ebruni.it>
2021-07-27 13:12:38 +0200

    Now the configuration element 'plugins' is compatible with Mojolicious 9.0. Add support for this parameter in previous versions

--- lib/Mojo/Leds.pm
+++ lib/Mojo/Leds.pm
@@ -7,34 +7,43 @@ sub startup() {
     my $s = shift;
 
     # log
-	$s->app->log( Mojo::Log->new(
-		path => $s->home->rel_file('/logs/app.log'),
-		level => 'info'
-	) ) unless ($s->app->mode eq 'development');;
+    $s->app->log(
+        Mojo::Log->new(
+            path  => $s->home->rel_file('/logs/app.log'),
+            level => 'info'
+        )
+    ) unless ( $s->app->mode eq 'development' );
 
     # plugins
-    $s->plugin(Config => {file => 'cfg/app.cfg'});
-    # lo leggo due volte per valorizzare dentro al cfg app->config->...
-    $s->plugin(Config => {file => 'cfg/app.cfg'});
+    $s->plugin( Config => { file => 'cfg/app.cfg' } );
 
-    if (my $plugins = $s->config->{plugins}) {
-        while (my ($k,$v) = each %$plugins) {
-            $s->plugin($k,$v);
+    # lo leggo due volte per valorizzare dentro al cfg app->config->...
+    $s->plugin( Config => { file => 'cfg/app.cfg' } );
+
+    # support for plugins config in Mojolicious < 9.0
+    if ( $Mojolicious::VERSION < 9 && ( my $plugins = $s->config->{plugins} ) ) {
+        die qq{Configuration value "plugins" is not an array reference}
+          unless ref $plugins eq 'ARRAY';
+        for my $plugin (@$plugins) {
+            die qq{Configuration value "plugins" contains an entry }
+              . qq{that is not a hash reference}
+              unless ref $plugin eq 'HASH';
+            $s->plugin( ( keys %$plugin )[0], ( values %$plugin )[0] );
         }
     }
 
-	# global configurations
+    # global configurations
     my $cfg = $s->config;
-	$s->secrets($cfg->{secret});
-	$s->sessions->default_expiration($cfg->{session}->{default_expiration});
-	$s->sessions->cookie_name($cfg->{session}->{name});
+    $s->secrets( $cfg->{secret} );
+    $s->sessions->default_expiration( $cfg->{session}->{default_expiration} );
+    $s->sessions->cookie_name( $cfg->{session}->{name} );
 
-	# la root dei file statici
+    # la root dei file statici
     my $docs_root = $s->config->{docs_root};
-	push @{$s->app->static->paths}, $s->home->rel_file("$docs_root/public");
-	# ridefinisco la root dei template
-	$s->app->renderer->paths->[0] =
-        $s->home->rel_file($docs_root)->to_string;
+    push @{ $s->app->static->paths }, $s->home->rel_file("$docs_root/public");
+
+    # ridefinisco la root dei template
+    $s->app->renderer->paths->[0] = $s->home->rel_file($docs_root)->to_string;
 }
 
 1;
